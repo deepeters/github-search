f48d1b2283986c8d26dd593814f167688ef1361b
Dennis Peters <dennis95peters@gmail.com>
2021-04-06 19:13:14 +0300

    This commit removes unnecessary lines of code

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-//import { url } from 'node:inspector';
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-//logo;string = "https://1000logos.net/wp-content/uploads/2018/11/GitHub-logo.png";
   constructor() { }
 
   ngOnInit() {
